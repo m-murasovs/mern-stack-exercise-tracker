0a86dd5ae78332734ae3198a1a529db378814ea6
Mihails Murasovs <m.murasovs@gmail.com>
2019-12-03 19:33:29 +0100

    Good navbar working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import styled from 'styled-components';
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Navbar} from './Components/Navbar';
@@ -17,10 +17,12 @@ const App = () => {
     <Router>
         <Navbar/>
         <BodyWrap>
+            <Switch>
             <Route path="/" exact component={ExercisesList} />
             <Route path="/edit/:id" component={EditExercise} />
             <Route path="/create" component={CreateExercise} />
             <Route path="/user" component={CreateUser} />
+            </Switch>
         </BodyWrap>
     </Router>
   );
