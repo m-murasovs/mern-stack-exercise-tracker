eb36e1fc41bebbdeaec85e9c3ac35ec1d16337e9
Mihails Murasovs <m.murasovs@gmail.com>
2019-12-05 14:26:28 +0100

    Ready to merge

--- src/Components/ExercisesList.js
+++ src/Components/ExercisesList.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 // import { ExerciseContext } from '../Contexts/ExerciseContext';
-// import styled from 'styled-components';
 // import { UserContext } from '../Contexts/UserContext';
+// import styled from 'styled-components';
 
 const ExercisesList = () => {
-    // const { exercises } = useContext(ExerciseContext);
+    const { exercises } = useContext(ExerciseContext);
+
+    const [ exercise, setExercise ] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/exercises')
+            .then(res => {
+                if (res.data.length > 0) {
+                    console.log(res.data)
+                    setExercises(res.data.map(exr => exr));
+                }
+            })
+            .catch(err => console.log("Problem retrieving exercises.", err))
+    }, []);
+
     // const { users } = useContext(UserContext);
     return (
         <div>
