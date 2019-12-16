7151545ddda9d417bde12d1d26da2841a31c152d
Mihails Murasovs <m.murasovs@gmail.com>
2019-12-16 19:51:12 +0100

    Make menu presentable

--- src/Components/MenuParts/Menu/Menu.styled.js
+++ src/Components/MenuParts/Menu/Menu.styled.js
@@ -3,29 +3,31 @@ import styled from 'styled-components';
 export const StyledMenu = styled.nav`
   background: ${({ theme }) => theme.primaryDark};
   color: ${({ theme }) => theme.primaryLight};
-  transform: ${({ open }) => open ? 'translateX(0%)' : 'translateX(200%)'};
-  text-align: center;
+  transform: ${({ open }) => open ? 'translateY(0%)' : 'translateY(-200%)'};
+  opacity: ${({ open }) => open ? '1' : '0'};
+  text-align: right;
+  padding: 3em 3em;
   position: absolute;
   right: 0;
   transition: transform 0.3s ease-in-out;
-  width: 50%;
+  width: 40%;
   border-radius: 4px;
 
   @media (max-width: ${({ theme }) => theme.mobile}) {
       width: 100%;
-      transform: ${({ open }) => open ? 'translateX(0%)' : 'translateX(200%)'};
+      transform: ${({ open }) => open ? 'translateY(0%)' : 'translateY(-200%)'};
     }
 
   a {
-    font-size: 2rem;
+    font-size: 3vw;
     text-transform: uppercase;
     padding: 2rem 0;
-    color: white;
+    color: ${({ theme }) => theme.primaryLight};
     text-decoration: none;
     transition: color 0.3s linear;
 
     @media (max-width: ${({ theme }) => theme.mobile}) {
-      font-size: 1.5rem;
+      font-size: 4vw;
       text-align: center;
     }
 
