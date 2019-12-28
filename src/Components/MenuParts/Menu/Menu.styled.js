66c95e1dd86b57e062dbfdb94db94607fd723eab
Mihails Murasovs <m.murasovs@gmail.com>
2019-12-28 21:32:10 +0100

    Update menu color

--- src/Components/MenuParts/Menu/Menu.styled.js
+++ src/Components/MenuParts/Menu/Menu.styled.js
@@ -6,12 +6,11 @@ export const StyledMenu = styled(animated.nav)`
 background: ${({ theme }) => theme.primaryDark};
 color: ${({ theme }) => theme.primaryLight};
 text-align: right;
-opacity: 0.8;
 padding: 3em 3em;
 position: absolute;
 right: 0;
+margin-top: 10px;
 width: 40%;
-border-radius: 4px;
 
   @media (max-width: ${({ theme }) => theme.mobile}) {
       width: 100%;
